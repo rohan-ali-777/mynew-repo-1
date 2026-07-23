4c256795c0eb4fcdc5d9c8ca01444793277d2423
Amir-sohail-sheik <sheikhamir036@gmail.com>
2026-07-23 21:19:43 +0530

    This is The Final Version Of Main App

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-const myH1 = document.createElement("h1");
-myH1.textContent = "This is the second file in my new Git Repo.";
-const body = document.querySelector("body");
-body.appendChild(myH1);
+
